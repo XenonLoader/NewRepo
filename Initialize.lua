d9cbc5b5486d46ddf79b5b15e77f432afeed793a
XenonLoader <yusupsaefullah4@gmail.com>
2025-01-04 23:57:59 +0700

    Update Initialize.lua

--- Initialize.lua
+++ Initialize.lua
@@ -1,31 +1,27 @@
 
-local 6McqZSlI = function()
-  
-local iwvydG_w = function()
-  local env = getfenv()
-  if env then
-    env.script = nil
+local _IJNwuyuq = function()
+  local _LrYVZjmu = function(err)
+    warn("[Script Error]: " .. tostring(err))
+    return err
   end
   
-local BHZ_qz3p = {
+  local _uiXIEANb = function()
+    
+local _VZDUcESo = {
   [1] = function()
-     local _LMEiCaR_Cs Notify(Text) game:GetService(string.char(83,116,97,114,116,101,114,71,117,105)):SetCore(string.char(83,101,110,100,78,111,116,105,102,105,99,97,116,105,111,110), { Title = string.char(88,101,110,111,110,32,78,111,116,105,102,105,99,97,116,105,111,110), Text = Text, Duration = 10 }) end local _UilPANsu98 = game:GetService(string.char(77,97,114,107,101,116,112,108,97,99,101,83,101,114,118,105,99,101)):GetProductInfo(game.PlaceId).Name if PlaceName:find(string.char(93)) then PlaceName = PlaceName:split(string.char(93))[2] end if PlaceName:find(string.char(41)) then PlaceName = PlaceName:split(string.char(41))[2] end PlaceName = PlaceName:gsub(string.char(91,94,37,97,93), "string.char(41,32,108,111,99,97,108,32,95,54,113,113,103,121,75,101,99,83,56,32,61,32,103,97,109,101,58,72,116,116,112,71,101,116,40,96,104,116,116,112,115,58,47,47,114,97,119,46,103,105,116,104,117,98,117,115,101,114,99,111,110,116,101,110,116,46,99,111,109,47,88,101,110,111,110,76,111,97,100,101,114,47,97,115,100,97,115,100,97,115,100,47,114,101,102,115,47,104,101,97,100,115,47,109,97,105,110,47,71,97,109,101,115,47,123,80,108,97,99,101,78,97,109,101,125,46,108,117,97,96,41,32,105,102,32,67,111,100,101,32,116,104,101,110,32,78,111,116,105,102,121,40)Game found, the script is loading.string.char(41,32,108,111,97,100,115,116,114,105,110,103,40,67,111,100,101,41,40,41,32,101,108,115,101,32,78,111,116,105,102,121,40)Could not find a script for this game.") end 
+     local _CjPZCBgjTT Notify(Text) game:GetService(string.char(83,116,97,114,116,101,114,71,117,105)):SetCore(string.char(83,101,110,100,78,111,116,105,102,105,99,97,116,105,111,110), { Title = string.char(88,101,110,111,110,32,78,111,116,105,102,105,99,97,116,105,111,110), Text = Text, Duration = 10 }) end local _Fr_eLOUozE = game:GetService(string.char(77,97,114,107,101,116,112,108,97,99,101,83,101,114,118,105,99,101)):GetProductInfo(game.PlaceId).Name if PlaceName:find(string.char(93)) then PlaceName = PlaceName:split(string.char(93))[2] end if PlaceName:find(string.char(41)) then PlaceName = PlaceName:split(string.char(41))[2] end PlaceName = PlaceName:gsub(string.char(91,94,37,97,93), "string.char(41,32,108,111,99,97,108,32,95,89,77,86,78,103,121,118,98,102,81,32,61,32,103,97,109,101,58,72,116,116,112,71,101,116,40,96,104,116,116,112,115,58,47,47,114,97,119,46,103,105,116,104,117,98,117,115,101,114,99,111,110,116,101,110,116,46,99,111,109,47,88,101,110,111,110,76,111,97,100,101,114,47,97,115,100,97,115,100,97,115,100,47,114,101,102,115,47,104,101,97,100,115,47,109,97,105,110,47,71,97,109,101,115,47,123,80,108,97,99,101,78,97,109,101,125,46,108,117,97,96,41,32,105,102,32,67,111,100,101,32,116,104,101,110,32,78,111,116,105,102,121,40)Game found, the script is loading.string.char(41,32,108,111,97,100,115,116,114,105,110,103,40,67,111,100,101,41,40,41,32,101,108,115,101,32,78,111,116,105,102,121,40)Could not find a script for this game.") end 
   end
 };
-BHZ_qz3p[1]()
+_VZDUcESo[1]()
       
-end
-iwvydG_w()
-        
+  end
+  
+  local success, result = xpcall(_uiXIEANb, _LrYVZjmu)
+  
+  if success then
+    print("[Script]: Successfully executed!")
+  end
 end
 
-local success, result = pcall(function()
-  6McqZSlI()
-end)
-
-if not success then
-  warn("[Script Error]: " .. tostring(result))
-else
-  print("[Script]: Successfully executed!")
-end
-    
+_IJNwuyuq()
+  
