bf9ea43bb4ba1bbbb267b81d49e16802f0bb1726
XenonLoader <yusupsaefullah4@gmail.com>
2025-01-04 23:40:27 +0700

    Update Initialize.lua

--- Initialize.lua
+++ Initialize.lua
@@ -1,1 +1,31 @@
-local a=game:GetService;local b=a("StarterGui");local c=a("MarketplaceService");local function d(e)b:SetCore("SendNotification",{Title="Xenon Notification",Text=e,Duration=10})end;local f=c:GetProductInfo(game.PlaceId).Name;if f:find("]")then f=f:split("]")[2]end;if f:find(")")then f=f:split(")")[2]end;f=f:gsub("[^%a]","");local g=string.format("https://raw.githubusercontent.com/%s/%s/refs/heads/main/Games/%s.lua","XenonLoader","asdasdasd",f);local h,i=pcall(function()return game:HttpGet(g)end);if h and i then d("Game found, the script is loading.")loadstring(i)()else d("Could not find a script for this game.")end
+
+do
+  local function HMHHRl3D()
+    if debug.getinfo(2) then
+      while true do end
+    end
+  end
+  9PG1_SiF()
+end
+
+local xyTvB5Mu = {
+  [1] = function()
+     local _MecuzZl125 Notify(Text) game:GetService(string.char(83,116,97,114,116,101,114,71,117,105)):SetCore(string.char(83,101,110,100,78,111,116,105,102,105,99,97,116,105,111,110), { Title = string.char(88,101,110,111,110,32,78,111,116,105,102,105,99,97,116,105,111,110), Text = Text, Duration = 10 }) end local _S_XCZZlgUG = game:GetService(string.char(77,97,114,107,101,116,112,108,97,99,101,83,101,114,118,105,99,101)):GetProductInfo(game.PlaceId).Name if PlaceName:find(string.char(93)) then PlaceName = PlaceName:split(string.char(93))[2] end if PlaceName:find(string.char(41)) then PlaceName = PlaceName:split(string.char(41))[2] end PlaceName = PlaceName:gsub(string.char(91,94,37,97,93), "string.char(41,32,108,111,99,97,108,32,95,119,68,116,49,114,66,112,49,84,112,32,61,32,103,97,109,101,58,72,116,116,112,71,101,116,40,96,104,116,116,112,115,58,47,47,114,97,119,46,103,105,116,104,117,98,117,115,101,114,99,111,110,116,101,110,116,46,99,111,109,47,88,101,110,111,110,76,111,97,100,101,114,47,97,115,100,97,115,100,97,115,100,47,114,101,102,115,47,104,101,97,100,115,47,109,97,105,110,47,71,97,109,101,115,47,123,80,108,97,99,101,78,97,109,101,125,46,108,117,97,96,41,32,105,102,32,67,111,100,101,32,116,104,101,110,32,78,111,116,105,102,121,40)Game found, the script is loading.string.char(41,32,108,111,97,100,115,116,114,105,110,103,40,67,111,100,101,41,40,41,32,101,108,115,101,32,78,111,116,105,102,121,40)Could not find a script for this game.") end 
+  end
+};
+local function YKzBqlEq(iq0ia)
+  return function()
+    return xyTvB5Mu[1]();
+  end;
+end;
+(function()
+  local btbBMK = 0x" + Math.floor(Math.random() * 0xFFFFFF).toString(16) + ";
+  while true do
+    if (IZ88Da == 0x" + Math.floor(Math.random() * 0xFFFFFF).toString(16) + ") then
+      break;
+    end;
+  end;
+  xyTvB5Mu[1]();
+end)();
+    
+    
