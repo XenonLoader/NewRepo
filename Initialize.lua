0281f1e6573fb810f2319c4425da066d9f9c479e
XenonLoader <yusupsaefullah4@gmail.com>
2025-01-05 00:02:38 +0700

    Update Initialize.lua

--- Initialize.lua
+++ Initialize.lua
@@ -1,27 +1,48 @@
-
 local _IJNwuyuq = function()
-  local _LrYVZjmu = function(err)
-    warn("[Script Error]: " .. tostring(err))
-    return err
-  end
-  
-  local _uiXIEANb = function()
-    
-local _VZDUcESo = {
-  [1] = function()
-     local _CjPZCBgjTT Notify(Text) game:GetService(string.char(83,116,97,114,116,101,114,71,117,105)):SetCore(string.char(83,101,110,100,78,111,116,105,102,105,99,97,116,105,111,110), { Title = string.char(88,101,110,111,110,32,78,111,116,105,102,105,99,97,116,105,111,110), Text = Text, Duration = 10 }) end local _Fr_eLOUozE = game:GetService(string.char(77,97,114,107,101,116,112,108,97,99,101,83,101,114,118,105,99,101)):GetProductInfo(game.PlaceId).Name if PlaceName:find(string.char(93)) then PlaceName = PlaceName:split(string.char(93))[2] end if PlaceName:find(string.char(41)) then PlaceName = PlaceName:split(string.char(41))[2] end PlaceName = PlaceName:gsub(string.char(91,94,37,97,93), "string.char(41,32,108,111,99,97,108,32,95,89,77,86,78,103,121,118,98,102,81,32,61,32,103,97,109,101,58,72,116,116,112,71,101,116,40,96,104,116,116,112,115,58,47,47,114,97,119,46,103,105,116,104,117,98,117,115,101,114,99,111,110,116,101,110,116,46,99,111,109,47,88,101,110,111,110,76,111,97,100,101,114,47,97,115,100,97,115,100,97,115,100,47,114,101,102,115,47,104,101,97,100,115,47,109,97,105,110,47,71,97,109,101,115,47,123,80,108,97,99,101,78,97,109,101,125,46,108,117,97,96,41,32,105,102,32,67,111,100,101,32,116,104,101,110,32,78,111,116,105,102,121,40)Game found, the script is loading.string.char(41,32,108,111,97,100,115,116,114,105,110,103,40,67,111,100,101,41,40,41,32,101,108,115,101,32,78,111,116,105,102,121,40)Could not find a script for this game.") end 
-  end
-};
-_VZDUcESo[1]()
-      
-  end
-  
-  local success, result = xpcall(_uiXIEANb, _LrYVZjmu)
-  
-  if success then
-    print("[Script]: Successfully executed!")
-  end
+    local _LrYVZjmu = function(err)
+        warn("\91Script Error\93: " .. tostring(err))
+        return err
+    end
+
+    local _uiXIEANb = function()
+        local _VZDUcESo = {
+            [1] = function()
+                -- Deklarasi fungsi Notify
+                local _CjPZCBgjTT = function(_WmJaIptR)
+                    game:GetService("\83\116\97\114\116\101\114\71\117\105"):SetCore("\83\101\110\100\78\111\116\105\102\105\99\97\116\105\111\110", {
+                        Title = "\88\101\110\111\110\32\78\111\116\105\102\105\99\97\116\105\111\110",
+                        Text = _WmJaIptR,
+                        Duration = 10
+                    })
+                end
+
+                -- Ambil nama tempat
+                local _Fr_eLOUozE = game:GetService("\77\97\114\107\101\116\112\108\97\99\101\83\101\114\118\105\99\101"):GetProductInfo(game.PlaceId).Name
+                if _Fr_eLOUozE:find("\93") then
+                    _Fr_eLOUozE = _Fr_eLOUozE:split("\93")[2]
+                end
+                if _Fr_eLOUozE:find("\41") then
+                    _Fr_eLOUozE = _Fr_eLOUozE:split("\41")[2]
+                end
+
+                -- Ganti karakter dalam nama tempat
+                _Fr_eLOUozE = _Fr_eLOUozE:gsub("\91\94\37\97\93", "")
+
+                -- Pemberitahuan
+                _CjPZCBgjTT("\71\97\109\101\32\102\111\117\110\100\44\32\116\104\101\32\115\99\114\105\112\116\32\105\115\32\108\111\97\100\105\110\103.")
+            end
+        }
+
+        -- Panggil fungsi pertama dalam tabel
+        _VZDUcESo[1]()
+    end
+
+    -- Eksekusi fungsi dengan xpcall untuk menangkap error
+    local success, result = xpcall(_uiXIEANb, _LrYVZjmu)
+
+    if success then
+        print("\91Script\93: \83\117\99\99\101\115\115\102\117\108\108\121\32\101\120\101\99\117\116\101\100!")
+    end
 end
 
 _IJNwuyuq()
-  
